554af09e06f2e48e98a90462b85bac9f448cf23a
Ginno Taimal <ginno.taimal@epn.edu.ec>
2023-09-14 20:44:15 -0500

    refactor: Cambio de nombres y ubicacion

--- AutoAssist/app/src/main/java/com/example/autoassist/activity/InicioActivity.kt
+++ AutoAssist/app/src/main/java/com/example/autoassist/activity/InicioActivity.kt
@@ -1,11 +1,12 @@
-package com.example.autoassist
+package com.example.autoassist.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.autoassist.R
 
-class Registro : AppCompatActivity() {
+class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_inicio)
     }
 }
